8080db3c9704214761cabd6bf0ec1b0a8a260549
AbelZepeda <143646373+AbelZepeda@users.noreply.github.com>
2024-04-25 10:51:43 -0700

    conexion a mysql

--- Proyecto/Backend/app.js
+++ Proyecto/Backend/app.js
@@ -2,15 +2,44 @@ let express = require('express');
 let mysql = require('mysql');
 let cors = require('cors');
 
+//Conexion a MySQL
+let conexion = mysql.createConnection({
+    host: 'localhost', //localhost
+    user: 'root',
+    password: '',
+    database: 'pwdata'
+});
+
 
 let app = express();
 app.use(express.json()); //devolver al cliente
 app.use(cors()); //aceptar solicitudes de un servidor externo
 
+//Nos conectamos a MySQL
+conexion.connect(function(error){
+    if(error){
+        throw error;
+    }else{
+        console.log('Conectado a la base de datos');
+    }
+});
+
+
 //Rutas de acceso 
 app.get("/", function(req,res){
     res.send("Ruta de inicio");
 })
+
+//Seleccionamos todos los clientes
+app.get('/api/cientes',(req,res)=>{
+    conexion.query('SELECT * FROM cientes',(error,filas,)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(filas);
+        }
+    });
+});
 
 
 //Encender el servidor 
