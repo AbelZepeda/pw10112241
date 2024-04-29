3b0cd4fec10a04fee35f834d275b61ad2a9d4c39
Abel Zepeda <143646373+AbelZepeda@users.noreply.github.com>
2024-04-29 10:57:33 -0700

    Seleccionar cliente especifico

---
+++ Backend/app.js
@@ -0,0 +1,60 @@
+let express = require('express');
+let mysql = require('mysql');
+let cors = require('cors');
+
+//Conexion a MySQL
+let conexion = mysql.createConnection({
+    host: 'localhost', //localhost
+    user: 'root',
+    password: '',
+    database: 'pwdata'
+});
+
+
+let app = express();
+app.use(express.json()); //devolver al cliente
+app.use(cors()); //aceptar solicitudes de un servidor externo
+
+//Nos conectamos a MySQL
+conexion.connect(function(error){
+    if(error){
+        throw error;
+    }else{
+        console.log('Conectado a la base de datos');
+    }
+});
+
+
+//Rutas de acceso 
+app.get("/", function(req,res){
+    res.send("Ruta de inicio");
+})
+
+//Seleccionamos todos los clientes
+app.get('/api/cientes',(req,res)=>{
+    conexion.query('SELECT * FROM cientes',(error,filas,)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(filas);
+        }
+    });
+});
+
+//Seleccionamos un cliente en especifico
+app.get('/api/cientes/:id',(req,res)=>{
+    conexion.query('SELECT * FROM cientes where id=?',[req.params.id],(error,fila)=>{
+        if(error){
+            throw error;
+
+        }else{
+            res.send(fila);        }    
+    })
+});
+
+
+//Encender el servidor 
+let puerto = 3000;
+app.listen(puerto, function(){
+    console.log("Servidor escuchando puerto " + puerto);
+})
