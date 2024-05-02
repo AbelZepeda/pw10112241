a277d2a82f3e624ccbfe0a54a50e855e825626c3
Abel Zepeda <143646373+AbelZepeda@users.noreply.github.com>
2024-05-02 10:54:55 -0700

    Updates

--- Backend/app.js
+++ Backend/app.js
@@ -48,10 +48,63 @@ app.get('/api/cientes/:id',(req,res)=>{
             throw error;
 
         }else{
-            res.send(fila);        }    
+            res.send(fila);}    
     })
 });
 
+//Eliminar a un cliente
+app.delete('api/cientes/:id',(req,res)=>{
+    let id=req.params.id;
+    conexion.query('DELETE FROM cientes where id=?',[id],(error,resultados)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(filas);
+        }
+    })
+});
+
+//Insertar un nuevo cliente
+app.post('/api/cientes',(req,res)=>{
+    let data = {
+        id:req.body.id,
+        nombre:req.body.nombre,
+        apellido:req.body.apellido,
+        direccion:req.body.direccion,
+        telefono:req.body.telefono,
+        rfc:req.body.rfc,
+        curp:req.curp,
+        cp:req.body.cp,
+    }
+    let sql = 'INSERT INTO cientes SET ?';
+    conexion.query(sql,data,(error,resultado)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(resultado);
+        }
+    })
+});
+
+//Actualizar
+app.put('/api/cientes/:id',(req,res)=>{
+    let id =req.params.id;
+    let nombre=req.body.nombre;
+    let apellido=req.body.apellido;
+    let direccion=req.body.direccion;
+    let telefono=req.body.telefono;
+    let rfc=req.body.rfc;
+    let curp=req.body.curp;
+    let cp=req.body.cp;
+    let sql="UPDATE cientes SET nombre=?,apellido=?,direcciones=?,telefono=?,rfc=?,curp=?,cp=? WHERE id=?";
+    conexion.query(sql,[nombre,apellido,direccion,telefono,rfc,curp,cp,id]),(error,results)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(results);
+        }
+    }
+});
 
 //Encender el servidor 
 let puerto = 3000;
